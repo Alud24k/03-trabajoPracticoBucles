c97f2b6148e727ec3b57a1c899c392dbf68d289d
Alud24k <ifem.perez@gmail.com>
2023-12-28 22:02:13 -0300

    Se realizo un programa que pide la edad del usuario y si es mayor que 18: puede conducir, de lo contrario se le indica que es menor de edad y no puede obtener su licencia.

---
+++ js/ejercicio-1.js
@@ -0,0 +1,7 @@
+const edad = parseInt(prompt(`Ingrese su edad`));
+
+if (edad >= 18) {
+  alert(`Es mayor, puede obtener su licencia para conducir`);
+} else {
+  alert(`Usted es menor, no puede obtener una licencia para conducir`);
+}
